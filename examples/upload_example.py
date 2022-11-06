3b4f61222021c523b3d425092ba58b8f9e541e94
Artic <artic.admisoffi@gmail.com>
2022-11-06 09:47:05 +0100

    Create upload_example.py

---
+++ examples/upload_example.py
@@ -0,0 +1,14 @@
+from uptobox import (
+    Client
+)
+
+client = Client(
+    token="token"
+)
+
+@client.listen()
+def on_connect():
+    print(f"Connected to {client.user.name}")
+    client.upload("myfile.txt")
+
+client.login()
